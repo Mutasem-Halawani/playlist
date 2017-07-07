c5e5f593e2c00b7033d959723c1832cc32f17bca
mutasem <mutasem.halawani@gmail.com>
2017-07-07 23:33:10 +0300

    change song marquee name to the choosen song from the list

--- js/musicPlayer.js
+++ js/musicPlayer.js
@@ -92,6 +92,44 @@ class MusicPlayer{
         var audioPlayer = $('<audio>',{
             'class' : 'audio-player',
             controls : true
+//            ended : this.playNextSong
+//            ended : function(){
+//                var audioPlayer = $('audio.audio-player');
+//                var activeSong = $('audio.audio-player source.active');
+//                var nextSong = $('audio.audio-player source.active + source') || $('audio.audio-player source:first-child');
+//
+//                activeSong.className = '';
+//                nextSong.className = 'active';
+//
+//                console.log(audioPlayer);
+//
+//                console.log('song ended');
+//                this.play();
+//
+//                this.src = nextSong.src;
+//                this.play(); 
+////                audioPlayer[0].play();
+////
+////                audioPlayer[0].src = nextSong.src;
+////                audioPlayer[0].play(); 
+//            }
+//            
+//            audioTracks : true,
+//            textTracks : true
+//            'play' : this.changeIconToPause()
+//            play : function(){
+//                console.log('song played now');
+////                $('i#pause').addClass('show');
+////                $('i#play-album-music-player').removeClass('show');
+//            },
+//            pause : function(){
+//                console.log('song paused');
+////                $('i#play-album-music-player').addClass('show');
+////                $('i#pause').removeClass('show');
+//            }
+//            pause: this.changeIconToPause(),
+//            play: this.changeIconToPlay()
+            
         });
         audioPlayer.appendTo(playerSection);
        
@@ -128,6 +166,7 @@ class MusicPlayer{
         editIcon.appendTo(controls);
         
         this.buildMusicList(albumID);
+        this.playNextSong();
     }
     
     buildMusicList(albumID){
@@ -141,11 +180,31 @@ class MusicPlayer{
                 var list = $('ol.music-songs-list');
                 var listItem = $('<li>',{
                     'class' : 'music-songs-list-item',
-                    text : object[i].name
+                    text : object[i].name,
+                    click : function(){
+                        
+//                        alert(this.textContent);
+//                        console.log('clicked');
+                        var currentSongMarquee = $('h5.current-song');
+//                        var Marquee = currentSongMarquee[0];
+//                        var parsed = Marquee.textContent;
+//                        console.log(Marquee);
+//                        console.log(typeof(Marquee));
+////                        console.log(currentSongMarquee[0]);
+//                        console.log(parsed);
+                        currentSongMarquee[0].textContent = this.textContent;
+                    }
                 });
                 listItem.appendTo(list);
             }
-
+//            var songs = $('ol.music-songs-list li');
+//            console.log(songs);
+//            for(let i=0;i<songs.length;i++){
+//                this.addClickEventForSongs;
+//                }
+            
+            
+            
             var songMarquee = $('div.song-marquee');
             var firstSong = $('li.music-songs-list-item').first().text();
             var currentSong = $('<h5>',{
@@ -166,6 +225,13 @@ class MusicPlayer{
         });
     }
     
+//    addClickEventForSongs(i){
+//        var songs = $('ol.music-songs-list li');
+//        songs[i].on('click',function(){
+//            console.log('hi'); 
+//        });
+//    }
+//    
     deleteAlbum(){
         var cancelPopup = new CancelPopup('Are you sure?');
              cancelPopup.buildPopup();
@@ -222,16 +288,106 @@ class MusicPlayer{
         $('i#play-album-music-player').addClass('show');
         $('i#pause').removeClass('show');
         var audioPlayer = $('audio.audio-player')[0];
-//        console.log(audioPlayer);
         audioPlayer.pause();
     }
     
     changeIconToPause(){
         $('i#pause').addClass('show');
         $('i#play-album-music-player').removeClass('show');
-        var audioPlayer = $('audio.audio-player')[0];
+        var audioPlayer = $('audio.audio-player');
+//        var songs = audioPlayer.children('source');
+        audioPlayer[0].play();
+////        songs[0].play();
 //        console.log(audioPlayer);
-        audioPlayer.play();
+//        console.log(audioPlayer[0]);
+//        console.log(songs);
+//        console.log(songs[0]);
+//        console.log(songs[1]);
+//        console.log(songs[1]);
+//        console.log(audioPlayer.audioTracks());
+
+
+
+        
+//        for (var i = 0; i < songs.length; i++) {
+//            audioListeners(i);
+//            console.log(songs.length);
+//        }
+//        function audioListeners(i) {
+//            songs[i].addEventListener('ended', function() {
+//              this.duration = 0; 
+//              this.pause();
+//              audioPlayer[i+1].play();
+//            }, false);
+//        }
+    }
+    
+    playNextSong(){
+//        var myvid = document.getElementById('myvideo');
+//
+//        myvid.addEventListener('ended', function(e) {
+//          // get the active source and the next video source.
+//          // I set it so if there's no next, it loops to the first one
+//          var activesource = document.querySelector("#myvideo source.active");
+//          var nextsource = document.querySelector("#myvideo source.active + source") || document.querySelector("#myvideo source:first-child");
+//
+//          // deactivate current source, and activate next one
+//          activesource.className = "";
+//          nextsource.className = "active";
+//
+//          // update the video source and play
+//          myvid.src = nextsource.src;
+//          myvid.play();
+//        });
+           var audioPlayer = $('audio.audio-player');
+           
+         
+            audioPlayer.on('ended',function(i){
+//                var firstSong = audioPlayer.children('source')[0];
+//                firstSong.className = 'active';
+//                console.log(firstSong);
+//                
+//                
+//                audioPlayer[0].src = 'songs/2pac/Tupac - Only God Can Judge Me.mp3';
+//                console.log(audioPlayer[0]);
+//                audioPlayer[0].play();
+//                var activeSong = $('audio.audio-player source.active');
+//                i = i;
+                var songsSources = $('audio.audio-player source');
+//                console.log(audioPlayer.children('source.active'));
+//                var songAttr = songsSources.attr('class');
+//                var activeSong = songsSources.attr('class') = 'active'
+//                if (songsSources.attr('class') === 'active'){
+//                    activeSong
+//                }
+//                var nextSong = audioPlayer.children('source.active + source');
+//                var nextSong = currentSong.next();
+//                
+//                console.log('currentSong', currentSong.src);
+//                console.log('nextSong', nextSong.src);
+                var activeSong = songsSources[0];
+                var nextSong = songsSources[2];
+                  nextSong.className = "active";
+//                console.log('activeSong', activeSong);
+//                console.log(activeSong.src);
+//                console.log('nextSong', activeSong);
+                console.log(nextSong.src);
+//                console.log(nextSong.src);
+//                console.log('nextSong', nextSong);
+//                console.log(nextSong);
+//                activeSong.className = '';
+//                nextSong.className = 'active';
+                audioPlayer[0].src = nextSong.src;
+//                console.log(audioPlayer);
+                console.log(audioPlayer[0]);
+//                console.log(audioPlayer.src);
+                audioPlayer[0].play();
+//                    i++;
+//                audioPlayer[0].src = nextSong.src;
+//                audioPlayer[0].play();
+            });
+           
+           
     }
     
     addNewPlaylist(){
